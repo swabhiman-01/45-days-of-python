e0bd149ea06fe64282a45f5059871c80682c8a10
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-07 16:33:24 +0530

    Add files via upload
    Day-1 Python user input programs

---
+++ input.py
@@ -0,0 +1,9 @@
+rollno = int(input("enter Any Rollno"))
+name = input ("enter student Name")
+gender = input("enter student Gender")
+mark = float(input("enter student Total mark secure marks"))
+
+print (type(rollno),rollno)
+print (type(name),name)
+print (type(gender),gender)
+print (type(mark),mark)
