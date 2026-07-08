e52c36e4d634b048fb218dd0e5d3163f20d28dd9
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-08 15:25:28 +0530

    Add files via upload
    Day 2: Added add and swap Python programs

---
+++ add.py
@@ -0,0 +1,11 @@
+#swap to find sum & average of any three numbers where number is given by user
+no1 = int (input ("enter no1 value"))
+no2 = int (input ("enter no2 value"))
+no3 = int (input ("enter no3 value"))
+
+res = no1 + no2 + no3
+avg = res/3
+
+print ("sum of three numbers =", res)
+print ("average of three number =", avg)
+
---
+++ swap.py
@@ -0,0 +1,5 @@
+no1 = int (input("enter no1 value"))
+no2 = int (input("enter no2 value"))
+print (f'before swap no1={no1} and no2={no2}')
+no1 , no2 = no2 , no1
+print (f'after swap no1={no1} and no2={no2}')
