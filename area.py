6d8244046d21ce14a69314d3e4dbf13b68cfade8
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-09 15:35:45 +0530

    Uploaded Day 3 Python programs to find the maximum of two numbers and calculate the area of a circle using user input and basic Python concepts.
    Uploaded Day 3 Python programs to find the maximum of two numbers and calculate the area of a circle using user input and basic Python concepts.

---
+++ area.py
@@ -0,0 +1,4 @@
+#swap to find area of a circle where radious value is given by user
+r = float(input("enter the value of r:"))
+a = 3.14 * r * r
+print("area of a circle =", a)
---
+++ max.py
@@ -0,0 +1,6 @@
+no1 = int (input("enter no1 value:"))
+no2 = int (input("enter no2 value:"))
+if no1 > no2 :
+    print (no1, "is max from", no2)
+else :
+    print (no2, "is max from", no1)
