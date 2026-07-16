05cbf9bc598d176da12642a15e03c48dbfceba22
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-16 21:26:23 +0530

    DAY-9

---
+++ swapusingArithmeticoperator.py
@@ -0,0 +1,8 @@
+n1= int (input ("Enter no1 value "))
+n2 = int (input ("Enter no2 value "))
+print ("before swap no1 =", n1, "& no2 =", n2)
+n1 = n1 + n2
+n2 = n1 - n2 
+n1 = n1 - n2   
+print ("After swap no1 =", n1, "& no2 =", n2)
+
---
+++ swapusingThirdvariable.py
@@ -0,0 +1,7 @@
+n1= int (input ("Enter no1 value "))
+n2 = int (input ("Enter no2 value "))
+print ("before swap no1 =", n1, "& no2 =", n2)
+temp = n1 
+n2 = n1 
+n1 = n2 
+print ("After swap no1 =", n1, "& no2 =", n2)
