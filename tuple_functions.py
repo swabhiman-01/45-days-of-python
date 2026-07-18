b48f4f67f66bebc0fa2e2e0141e50761bea80cb7
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-18 20:06:56 +0530

    DAY-11

---
+++ tuple_functions.py
@@ -0,0 +1,14 @@
+obj = (10, 20, -10, 34, 45, -7, 0)
+
+print("List elements:", obj)
+print("Number of elements:", len(obj))
+print("Maximum element:", max(obj))
+print("Minimum element:", min(obj))
+print("Sum of elements:", sum(obj))
+
+print("List in Ascending Order:", sorted(obj))
+print("List in Descending Order:", sorted(obj, reverse=True))
+
+print(obj)
+del(obj)
+print(obj)
