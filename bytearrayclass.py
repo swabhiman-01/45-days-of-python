0c0dc0692506d635ffbab7859d80bddb1f9ba4d2
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-21 18:14:15 +0530

    DAY-14

---
+++ bytearrayclass.py
@@ -0,0 +1,8 @@
+obj1 = bytearray()
+print(len(obj1),obj1)
+obj2 = bytearray([10,20,30,2,10,])
+print(len(obj2),obj2)
+print(obj2[2])
+obj2[1] = 100
+print(obj2[1])
+
---
+++ bytesclass.py
@@ -0,0 +1,7 @@
+obj1 = bytes()
+print(len(obj1),obj1)
+obj2 = bytes([10,20,30,2,10,])
+print(len(obj2),obj2)
+print(obj2[2])
+obj2[1] = 100
+
