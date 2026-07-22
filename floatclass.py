f4b31541c46f9aa2e73c0c74e091d3b6e8ce5620
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-22 21:06:56 +0530

    DAY-15

---
+++ floatclass.py
@@ -0,0 +1,8 @@
+obj = float ()
+print ( obj , type ( obj ))
+
+obj = float ( 1.2 )
+print ( obj , type ( obj ) )
+
+obj = 11.22
+print ( obj , type ( obj ) )
---
+++ intclass.py
@@ -0,0 +1,30 @@
+obj = int ()
+print ( obj, type ( obj ) )
+
+obj = int ( 10 )
+print ( obj, type ( obj ) )
+
+obj = int ( "101" , base = 10  )
+print ( obj , type ( obj ))
+
+obj = int ( "101" , base = 2 )
+print ( obj , type ( obj) )
+
+obj = int ( "101" , base = 8 )
+print ( obj , type ( obj ))
+
+obj = int ( "101" , base = 16 )
+print ( obj , type ( obj ) )
+
+obj = 10
+print ( obj , type ( obj ))
+
+obj = 0b101
+print ( obj , type ( obj ) )
+
+obj = 0o101
+print ( obj , type ( obj ))
+
+obj = 0x101
+print ( obj , type ( obj ))
+	
