9f6b049e5dbf920aecd49457672881390a8274ec
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-26 17:34:25 +0530

    DAY-19

---
+++ dict_functions.py
@@ -0,0 +1,7 @@
+obj = {"regno":1,'fname':"rajni",'lname':"kant","contno":6370142412,"math":78,"phy":81,"chem":45,'gender':"male"}
+print(len(obj))
+print(sorted(obj))
+print(sorted(obj,reverse = True))
+print(max(obj))
+print(min(obj))
+del(obj)
---
+++ dict_operators.py
@@ -0,0 +1,12 @@
+obj1 = {"regno":1,'fname':"rajni"}
+obj2 = {"regno":2,'fname':"raj"}
+print("rajni" in obj1)
+print('fname' in obj1)
+print("rajni" not in obj2)
+print('fname' not in obj2)
+print(obj1 is obj2)
+print(obj1 is not obj2)
+obj3 = obj1
+print(obj1 is obj3)
+print(obj1 == obj2)
+print(obj1 != obj2)
