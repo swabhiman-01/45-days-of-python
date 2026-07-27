07358367dd8bc36927ce9f590b897846dc95ad3b
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-27 18:33:02 +0530

    DAY--20

---
+++ if else.py
@@ -0,0 +1,8 @@
+#WAP to check a number is negative or positive
+no = int(input("Enter any number"))
+if no < 0:
+    print("number is negative")
+    print("hii")
+else :
+    print("number is positive")
+print("program ended")
---
+++ if.py
@@ -0,0 +1,4 @@
+#WAP to check a number is negative
+no = int(input("enter any number:"))
+if no < 0:
+    print("negative")
