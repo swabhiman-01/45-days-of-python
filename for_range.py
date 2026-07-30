55be01aae0c3822c0cec050bba12817db5d9a05f
swabhiman rout <swabhimanrout58@gmail.com>
2026-07-30 19:52:30 +0530

    DAY-23

---
+++ for_range.py
@@ -0,0 +1,17 @@
+# Python Program to demonstrate range() in for loop
+for no in range(10):
+    print(no, end=" ")
+print("\n")
+
+for no in range(2, 10):
+    print(no, end=" ")
+print("\n")
+
+for no in range(2, 10, 2):
+    print(no, end=" ")
+print("\n")
+
+
+for no in range(10, 0, -1):
+    print(no, end=" ")
+print("\n")
